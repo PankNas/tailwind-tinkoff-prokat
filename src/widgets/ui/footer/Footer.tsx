a07308fc97439c128873ab1b8bc4a8c9412456e0
a.pankratova <a.pankratova@digitalms.ru>
2024-09-26 21:24:53 +0300

    feat: добавит шрифты

--- src/widgets/ui/footer/Footer.tsx
+++ src/widgets/ui/footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="text-white/40 text-center pb-4 shrink">
+    <footer className="text-white/40 text-center pb-4 shrink text-xs">
       © {currentYear} Тинькофф Прокат
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+    },
     extend: {
       colors: {
         white: '#FFFFFF',
@@ -12,10 +12,9 @@ export default {
         black: {
           800: '#1C1C1C',
           900: '#000',
-        }
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
